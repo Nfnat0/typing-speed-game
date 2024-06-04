35c45e48fcca17034c054ea1d7e088d87b13812b
nfnat0 <mjtmwm@gmail.com>
2024-06-04 21:33:06 +0900

    chore: Update styling for game screen and input field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import StartScreen from './components/StartScreen';
 import GameScreen from './components/GameScreen';
 import ResultScreen from './components/ResultScreen';
