45acd008cd22b8aa603fc3b00d21767f07370147
nfnat0 <mjtmwm@gmail.com>
2024-06-07 18:08:35 +0900

    style: Update StartScreen dropdown and input container styling

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -26,26 +26,28 @@ const StartScreen = ({ onStart }) => {
       </div>
       <div className="main">
         <p>Welcome to the Typing Speed Game. Test your typing skills and improve your speed. Select a genre and the number of repetitions, then press play to start the game.</p>
-        <div className="dropdown-container">
-          <label htmlFor="genre">Select Genre : </label>
-          <select className="genre-dropdown" value={genre} onChange={handleChangeGenre}>
-            <option value="sayings">Sayings</option>
-            <option value="news">News</option>
-            <option value="programming">Programming</option>
-            <option value="jokes">Jokes</option>
-            <option value="advice">Advice</option>
-          </select>
-        </div>
-        <div className="input-container">
-          <label htmlFor="repetitions">Repetitions (1-10):</label>
-          <input
-            type="number"
-            id="repetitions"
-            value={repetitions}
-            onChange={handleChangeRepetitions}
-            min="1"
-            max="10"
-          />
+        <div className="containers">
+          <div className="dropdown-container">
+            <label htmlFor="genre">Select Genre : </label>
+            <select className="genre-dropdown" value={genre} onChange={handleChangeGenre}>
+              <option value="sayings">Sayings</option>
+              <option value="news">News</option>
+              <option value="programming">Programming</option>
+              <option value="jokes">Jokes</option>
+              <option value="advice">Advice</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="repetitions">Repetitions (1-10) : </label>
+            <input
+              type="number"
+              id="repetitions"
+              value={repetitions}
+              onChange={handleChangeRepetitions}
+              min="1"
+              max="10"
+            />
+          </div>
         </div>
       </div>
       <div className="buttons">
