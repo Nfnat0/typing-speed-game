5a93c6175146d64a30c494d51d250a4b67a95090
nfnat0 <mjtmwm@gmail.com>
2024-06-07 13:22:46 +0900

    feat: Improve score calculation in scoreCalculator.js

--- src/scoreCalculator.js
+++ src/scoreCalculator.js
@@ -1,6 +1,6 @@
 export const calculateScore = (elapsedTime, totalLetters, correctLetters) => {
     const accuracy = correctLetters / totalLetters;
     const accuracyBonus = accuracy * 100;
-    const timeBonus = 100 / elapsedTime;
+    const timeBonus = correctLetters / elapsedTime;
     return Math.round(accuracyBonus * timeBonus);
     };
