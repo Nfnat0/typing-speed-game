2c8fb6801bce385461e7f46fc02817cbb1bcb36f
nfnat0 <mjtmwm@gmail.com>
2024-06-11 01:03:40 +0900

    chore: Update sentenceFetcher.js to use double quotes consistently

--- src/sentenceFetcher.js
+++ src/sentenceFetcher.js
@@ -1,47 +1,47 @@
 export const fetchSentence = async (genre) => {
   try {
-    let url = '';
+    let url = "";
 
     switch (genre) {
-      case 'sayings':
-        url = 'https://api.quotable.io/random';
+      case "sayings":
+        url = "https://api.quotable.io/random";
         break;
-      case 'news':
+      case "news":
         // Use a free news API that doesn't require registration, such as the mock news API for example purposes
-        url = 'https://inshortsapi.vercel.app/news?category=technology';
+        url = "https://inshortsapi.vercel.app/news?category=technology";
         break;
-      case 'programming':
-        url = 'https://programming-quotes-api.herokuapp.com/quotes/random';
+      case "programming":
+        url = "https://programming-quotes-api.herokuapp.com/quotes/random";
         break;
-      case 'jokes':
-        url = 'https://v2.jokeapi.dev/joke/Any';
+      case "jokes":
+        url = "https://v2.jokeapi.dev/joke/Any";
         break;
-      case 'advice':
-        url = 'https://api.adviceslip.com/advice';
+      case "advice":
+        url = "https://api.adviceslip.com/advice";
         break;
       default:
-        throw new Error('Invalid genre');
+        throw new Error("Invalid genre");
     }
-  
+
     const response = await fetch(url);
     const data = await response.json();
-  
+
     switch (genre) {
-      case 'sayings':
+      case "sayings":
         return data.content;
-      case 'news':
+      case "news":
         return data.data[0].content;
-      case 'programming':
+      case "programming":
         return data.en;
-      case 'jokes':
+      case "jokes":
         return data.setup ? `${data.setup} - ${data.delivery}` : data.joke;
-      case 'advice':
+      case "advice":
         return data.slip.advice;
       default:
-        throw new Error('Invalid genre');
+        throw new Error("Invalid genre");
     }
   } catch (error) {
-    console.error('Error fetching sentence:', error);
+    console.error("Error fetching sentence:", error);
     return "Couldn't fetch sentence.";
   }
 };
