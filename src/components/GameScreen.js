7b263a9b50ff6d3a18d5404bb80db69cc6100091
nfnat0 <mjtmwm@gmail.com>
2024-06-11 21:19:59 +0900

    Revert "chore: Update GameScreen to flip characters horizontally and add spacing"
    This reverts commit 3d13e07b7751388901ff4cbcadb84bee6c3f9258.

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -158,12 +158,6 @@ const GameScreen = ({
 
   // Render the sentence with typed letters highlighted
   const renderSentence = () => {
-    const characterStyle = {
-      transform: "scale(-1, 1)", // Flip horizontally
-      display: "inline-block",
-      marginRight: "2px", // Optional: add some spacing between characters
-    };
-
     let typedIndex = 0;
     return sentences[currentSentenceIndex]?.split("").map((char, index) => {
       const isSpace = char === " ";
@@ -171,10 +165,7 @@ const GameScreen = ({
       if (!isSpace) typedIndex++;
 
       return (
-        <span
-          key={index}
-          style={{ ...characterStyle, color: isTyped ? "yellow" : "white" }}
-        >
+        <span key={index} style={{ color: isTyped ? "yellow" : "white" }}>
           {char}
         </span>
       );
